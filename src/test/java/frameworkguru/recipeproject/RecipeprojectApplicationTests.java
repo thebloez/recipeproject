90eda9ab89a6e6fa2798b2276b4a393d2796510a
bluscang <ryandunksfamz@gmail.com>
2017-11-21 05:15:42 +0700

    first commit

---
+++ src/test/java/frameworkguru/recipeproject/RecipeprojectApplicationTests.java
@@ -0,0 +1,16 @@
+package frameworkguru.recipeproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecipeprojectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
